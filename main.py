39dd43b6bc09c4fe3afc377a2baa51aa65f51715
Lucas Cabral Picoloto <132309988+Cabral-dev@users.noreply.github.com>
2023-10-10 16:15:50 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("What is the area of the retangle? ")
+
+base = float(input("What is the base? "))
+height = float(input("What is the height? "))
+
+area = base * height
+
+print(f"Your area is: {area} unit of measurement")
